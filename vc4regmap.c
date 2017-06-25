d6e25ba247e8f2a049c30272785b9b9635c7e8e8
Terminus <i.can.speak.c.and.basic@gmail.com>
2017-06-25 11:39:42 +0900

    Fix indent style by using EditorConfig

--- vc4regmap.c
+++ vc4regmap.c
@@ -10,39 +10,39 @@ static unsigned peri_addr, peri_size;
 
 void vc4regmap_init()
 {
-	bcm_host_init();
+    bcm_host_init();
 
-	mbfd = mailbox_open();
-	if (mailbox_qpu_enable(mbfd, 1)) {
-		fprintf(stderr, "%s:%d: Failed to enable QPU\n", __FILE__, __LINE__);
-		exit(EXIT_FAILURE);
-	}
+    mbfd = mailbox_open();
+    if (mailbox_qpu_enable(mbfd, 1)) {
+        fprintf(stderr, "%s:%d: Failed to enable QPU\n", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
 
-	peri_addr = bcm_host_get_peripheral_address();
-	peri_size = bcm_host_get_peripheral_size();
+    peri_addr = bcm_host_get_peripheral_address();
+    peri_size = bcm_host_get_peripheral_size();
 }
 
 void vc4regmap_finalize()
 {
-	mailbox_qpu_enable(mbfd, 0);
-	mailbox_close(mbfd);
-	bcm_host_deinit();
+    mailbox_qpu_enable(mbfd, 0);
+    mailbox_close(mbfd);
+    bcm_host_deinit();
 }
 
 volatile uint32_t* vc4regmap_map_peri()
 {
-	return map_on_cpu(BUS_TO_PHYS(peri_addr), peri_size);
+    return map_on_cpu(BUS_TO_PHYS(peri_addr), peri_size);
 }
 
 void vc4regmap_unmap_peri(volatile uint32_t *peri)
 {
-	unmap_on_cpu((void*) peri, peri_size);
+    unmap_on_cpu((void*) peri, peri_size);
 }
 
 _Bool is_qpu_enabled(volatile uint32_t *peri)
 {
-	const uint32_t expected = ('V' << 0) | ('3' << 8) | ('D' << 16);
-	if ((peri[V3D_IDENT0] & 0xffffff) == expected)
-		return !0;
-	return 0;
+    const uint32_t expected = ('V' << 0) | ('3' << 8) | ('D' << 16);
+    if ((peri[V3D_IDENT0] & 0xffffff) == expected)
+        return !0;
+    return 0;
 }
